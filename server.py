262e1985793ed66d83e43c23584b7865e7336a45
yochen603 <68061993+yochen603@users.noreply.github.com>
2024-04-17 11:46:53 -0400

    Add files via upload
    add score

--- server.py
+++ server.py
@@ -11,7 +11,15 @@ chart = qim['9']
 trade = qim['10']
 lessons = qim['lessons']
 quiz_results = []
-
+correct_answers = {
+    'question0': 'Triangle',
+    'question1': 'Head & Shoulders',
+    'question2': 'Cup & Handle',
+    'drop1': ['Head & Shoulders', 'Cup & Handle'],
+    'drop2': ['Descending Triangle', 'Reverse Pennant'],
+    'image6': 'buy',
+    'image7': 'sell'
+}
 # ROUTES
 @app.route('/')
 def welcome():
@@ -36,15 +44,35 @@ def introduction_trade():
 def quiz(id):
     return render_template('quiz.html', id=id, data=qim)
 
+
 @app.route('/submit_quiz', methods=['POST'])
 def submit_quiz():
     quiz_data = request.json
+
+    score = 0
+    for key in quiz_data:
+        user_answer = quiz_data[key]
+        correct_answer = correct_answers[key]
+        if isinstance(correct_answer, list):
+            if set(user_answer) == set(correct_answer):
+                score += 1
+        else:
+            if user_answer == correct_answer:
+                score += 1
+
+    quiz_data['score'] = score  # Add the 'score' key to the quiz_data dictionary
     quiz_results.append(quiz_data)
-    return jsonify({'message': 'Quiz submitted successfully'})
+
+    return jsonify({'message': 'Quiz submitted successfully', 'score': score})
 
 @app.route('/learn/0')
 def the_basics():
     return render_template('the_basics.html')
+
+@app.route('/quiz/score')
+def quiz_score():
+    score = quiz_results[-1]['score']  # Retrieve the score from the last submitted quiz
+    return render_template('score.html', score=score)
 
 if __name__ == '__main__':
     app.run(debug=True)
